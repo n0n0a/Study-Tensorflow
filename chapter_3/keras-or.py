5e9835e1b8ce80310589d1d19700079a27682382
n0n0a <n0n0a6262@gmail.com>
2020-10-17 14:26:22 +0900

    feat: keras or

---
+++ chapter_3/keras-or.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras.optimizers import SGD
+
+
+def main():
+    np.random.seed(0)  # 乱数シード
+
+    '''
+    モデル設定
+    '''
+    model = Sequential([
+        # Dense(input_dim=2, output_dim=1),  # Keras 1
+        Dense(input_dim=2, units=1),  # Keras 2
+        Activation('sigmoid')
+    ])
+
+    model.compile(loss='binary_crossentropy', optimizer=SGD(lr=0.1))
+
+    '''
+    モデル学習
+    '''
+    # ORゲート
+    X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+    Y = np.array([[0], [1], [1], [1]])
+
+    # model.fit(X, Y, nb_epoch=200, batch_size=1)  # Keras 1
+    model.fit(X, Y, epochs=200, batch_size=1)  # Keras 2
+
+    '''
+    学習結果の確認
+    '''
+    classes = model.predict_classes(X, batch_size=1)
+    prob = model.predict_proba(X, batch_size=1)
+
+    print('classified:')
+    print(Y == classes)
+    print()
+    print('output probability:')
+    print(prob)
+
+
+if __name__ == '__main__':
+    main()
