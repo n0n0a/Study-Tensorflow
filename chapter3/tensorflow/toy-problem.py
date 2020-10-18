d39043460e9259d451c591312b14cf9ecf9fdc07
Nao Goto <n0n0a6262@gmail.com>
2020-10-18 19:39:20 +0900

    feat: solve toy-problem (#7)

---
+++ chapter3/tensorflow/toy-problem.py
@@ -0,0 +1,60 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+
+def main():
+    np.random.seed(0)
+    tf.set_random_seed(1234)
+
+    N = 300
+    X, y = datasets.make_moons(N, noise=3)
+    Y = y.reshape(N,1)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.8)
+    num_hidden = 2
+    x = tf.placeholder(tf.float32, shape=[None,2])
+    t = tf.placeholder(tf.float32, shape=[None,1])
+    W = tf.Variable(tf.truncated_normal([2,num_hidden]))
+    b = tf.Variable(tf.zeros([num_hidden]))
+    h = tf.nn.sigmoid(tf.matmul(x,W)+b)
+
+    V = tf.Variable(tf.truncated_normal([num_hidden,1]))
+    c = tf.Variable(tf.zeros([1]))
+    y = tf.nn.sigmoid(tf.matmul(h,V)+c)
+
+    cross_entropy =  -tf.reduce_sum(t*tf.log(y)+(1-t)*tf.log(1-y))
+    train_step = tf.train.GradientDescentOptimizer(0.05).minimize(cross_entropy)
+    correct_prediction = tf.equal(tf.to_float(tf.greater(y, 0.5)),t)
+
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+
+    batch_size = 20
+    n_batches = N // batch_size
+
+    init = tf.global_variables_initializer()
+    sess = tf.Session()
+    sess.run(init)
+
+    for epoch in range(500):
+        X_, Y_ = shuffle(X_train, Y_train)
+        for i in range(n_batches):
+            start = i*batch_size
+            end = start+batch_size
+
+            sess.run(train_step, feed_dict={
+                x:X_[start:end],
+                t:Y_[start:end]
+            })
+
+    accuracy_rate = accuracy.eval(session=sess, feed_dict={
+        x:X_test,
+        t:Y_test
+    })
+
+    print('accuracy: ', accuracy_rate)
+
+
+if __name__ == '__main__':
+    main()
